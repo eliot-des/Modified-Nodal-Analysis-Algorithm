9fe0c00746259b0d5ef9bdcd82b833de89ce7937
Eliot Deschang <eliot.deschang@gmail.com>
2024-05-09 15:14:19 +0200

    Creation of a diode class

--- Modified_nodal_analysis_v2.4/component.cpp
+++ Modified_nodal_analysis_v2.4/component.cpp
@@ -73,13 +73,14 @@ VoltageSource::VoltageSource(unsigned start_node, unsigned end_node, double valu
     : Component(start_node, end_node, value), voltage(value), index(index) {}
 
 void VoltageSource::stamp(Netlist& netlist) const {
+    unsigned n = netlist.n;
 
-    netlist.A(start_node, netlist.n + index) =  1;
-    netlist.A(end_node,   netlist.n + index) = -1;
-    netlist.A(netlist.n + index, start_node) =  1;
-    netlist.A(netlist.n + index,   end_node) = -1;
+    netlist.A(start_node, n + index) =  1;
+    netlist.A(end_node,   n + index) = -1;
+    netlist.A(n + index, start_node) =  1;
+    netlist.A(n + index,   end_node) = -1;
 
-    netlist.b(netlist.n + index) = voltage;
+    netlist.b(n + index) = voltage;
 }
 
 
@@ -105,6 +106,7 @@ IdealOPA::IdealOPA(unsigned start_node, unsigned end_node, unsigned output_node,
 
 void IdealOPA::stamp(Netlist& netlist) const {
     unsigned n = netlist.n;
+
     netlist.A(output_node, n + index) = 1;
     netlist.A(n + index, start_node) = 1;
     netlist.A(n + index, end_node) = -1;
@@ -115,4 +117,51 @@ VoltageProbe::VoltageProbe(unsigned start_node, unsigned end_node)
 
 void VoltageProbe::getVoltage(Netlist& netlist) {
     value = netlist.x(start_node) - netlist.x(end_node);
+}
+
+
+Diode::Diode(unsigned start_node, unsigned end_node)
+	: Component(start_node, end_node, 0.0) {
+
+    N = 1.6;	   //ideality factor
+    Is = 2.6e-6;   //reverse saturation current
+
+    Vt = 0.025852;  //thermal voltage at approx. 300K 
+    N_Vt = N * Vt;  //N*Vt
+
+    Id = 0;         //current through the diode
+    voltage = 0;    //voltage across the diode
+
+    Ieq = 0;        //equivalent current
+    Geq = 0;	    //equivalent conductance
+}
+
+
+void Diode::stamp(Netlist& netlist) const {
+    unsigned n = netlist.n;
+
+    netlist.A(start_node, start_node) += Geq;
+    netlist.A(end_node,     end_node) += Geq;
+    netlist.A(start_node,   end_node) -= Geq;
+    netlist.A(end_node,   start_node) -= Geq;
+
+    netlist.b(start_node) -= Is;
+    netlist.b(end_node)   += Is;
+
+}
+    
+void Diode::update_voltage(Netlist& netlist) {
+	voltage = netlist.x(start_node) - netlist.x(end_node);
+}
+
+void Diode::update_Id(Netlist& netlist) {
+	Id = Is * std::expm1(voltage / N_Vt); //Id = Is * (std::exp(voltage / N_Vt) - 1);
+}
+
+void Diode::update_Geq(Netlist& netlist) {
+	Geq = (Is / N_Vt) * std::exp(voltage / N_Vt);
+}
+
+void Diode::update_Ieq(Netlist& netlist) {
+	Ieq = Id - Geq * voltage;
 }
